d6525be892acf7efc2a25401fd93f3ffcb83f29e
sanya-kenneth <sanyakenneth@gmail.com>
2025-05-22 19:34:05 +0300

    Add project

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 
-from .models import Comment, Incident
+from .models import Comment, Incident, Feedback
 
 
 class CommentForm(forms.ModelForm):
     comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'text-area-class form-control'}))
-    
+    act = forms.CharField(widget=forms.TextInput(attrs={"type":"hidden", "id":"modal-input", "name":"modal-input"}))
+
     class Meta:
         model = Comment
         fields = ('comment',)
-        
+
 
 class IncidentForm(forms.ModelForm):
     form_of_corruption = forms.CharField(max_length=150)
@@ -19,4 +20,21 @@ class IncidentForm(forms.ModelForm):
 
     class Meta:
         model = Incident
+        fields = '__all__'
+        
+
+class FeedbackForm(forms.ModelForm):
+    # form_of_corruption = forms.CharField(max_length=150)
+    # location = forms.CharField(max_length=150)
+    # description = forms.CharField(widget=forms.Textarea(attrs={'class': 'text-area-class form-control'}))
+    # email = forms.EmailInput()
+    name = forms.CharField(max_length=300, widget=forms.TextInput(attrs={"id":"name", "name":"name"}), required=False, label="Your Name(optional)")
+    email = forms.CharField(widget=forms.TextInput(attrs={"id":"email", "name":"email"}), required=False, label="Your Email(optional)")
+    feedback_type = forms.ChoiceField(widget=forms.Select(attrs={"id":"category", "name":"category"}), 
+                                      choices=Feedback.FEEDBACK_CHOICES)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'text-area-class form-control', 
+                                                           "placeholder":"Share your thoughts, suggestions, or report any issues..."}))
+
+    class Meta:
+        model = Feedback
         fields = '__all__'
